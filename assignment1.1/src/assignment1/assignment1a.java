d56eb20c0b843cd6190a1253ad031caa3712762b
Ramya <nramya1996@gmail.com>
2017-02-04 20:56:30 +0530

    assignmentjava1.1

---
+++ assignment1.1/src/assignment1/assignment1a.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class assignment1a {
+	public static void main(String[] args) {
+		int a = 19, b = 53, sum;
+		sum = a-(-b);
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("sum = "+sum);
+	}
+	}
